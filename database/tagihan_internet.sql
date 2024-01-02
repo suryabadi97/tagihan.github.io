fc2760a066100771e9fe16e6781db8c69fce94cd
suryabadi97 <155256579+suryabadi97@users.noreply.github.com>
2024-01-03 01:57:50 +0700

    Update tagihan_internet.sql

--- database/tagihan_internet.sql
+++ database/tagihan_internet.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Nov 21, 2021 at 09:17 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.34
+-- Host: 127.0.0.1
+-- Generation Time: Jan 02, 2024 at 04:37 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,7 +37,7 @@ CREATE TABLE `tb_kas` (
   `jenis_kas` varchar(15) NOT NULL,
   `status` int(11) NOT NULL,
   `id_tagihan` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -49,17 +49,19 @@ CREATE TABLE `tb_paket` (
   `id_paket` int(11) NOT NULL,
   `nama_paket` varchar(255) NOT NULL,
   `harga` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tb_paket`
 --
 
 INSERT INTO `tb_paket` (`id_paket`, `nama_paket`, `harga`) VALUES
-(1, '1MB PERSONAL', 50000),
-(2, '2MB PERSONAL', 100000),
-(3, '3mb', 300000),
-(4, '4mb', 300000);
+(3, '5 MBPs', 125000),
+(4, '4 MBPs', 110000),
+(5, '1 Devace', 30000),
+(6, '2 Devace', 60000),
+(7, '3 Devace', 90000),
+(8, '4 Devace', 100000);
 
 -- --------------------------------------------------------
 
@@ -73,17 +75,14 @@ CREATE TABLE `tb_pelanggan` (
   `alamat` varchar(255) NOT NULL,
   `no_telp` varchar(20) NOT NULL,
   `paket` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tb_pelanggan`
 --
 
 INSERT INTO `tb_pelanggan` (`id_pelanggan`, `nama_pelanggan`, `alamat`, `no_telp`, `paket`) VALUES
-(2, 'ADI SETYO N', 'Jalan A. Yani No. 02 Randublatung- Blora  Jateng', '6281315029099', 2),
-(4, 'Dading Hermawan', 'Randublatung', '6281315029099', 1),
-(5, 'AHMAD ALI', 'Jakarta', '6281315029099', 2),
-(6, 'RAFIKA DEWI, SE', 'jakarta', '6285781480396', 1);
+(7, 'rosi', 'Lubawang', '6282131024817', 3);
 
 -- --------------------------------------------------------
 
@@ -102,14 +101,14 @@ CREATE TABLE `tb_profile` (
   `foto` varchar(255) NOT NULL,
   `ktu` varchar(255) NOT NULL,
   `nip_ktu` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tb_profile`
 --
 
 INSERT INTO `tb_profile` (`nama_sekolah`, `alamat`, `telpon`, `website`, `kota`, `bendahara`, `nip`, `foto`, `ktu`, `nip_ktu`) VALUES
-('RT RW NET', 'Dukuh Jogo Rt/Rw 002/002 Desa. Gembyungan Kec. Randublatung Kab. Blora - Jawa Tengah\r\nTelp. 085781480396', '021.090939', 'www.sekolah.com', 'Jakarta', 'Bejo Santoso', '1968890993933434', 'admin.png', 'ABDUL MUIS', '343434343434');
+('SURYA ABADI', 'Lubawang, Kec. Banyuglugur, Kab. Situbondo', '021.090939', 'www.sekolah.com', 'Jakarta', 'Bejo Santoso', '1968890993933434', 'favicon.png', 'ABDUL MUIS', '343434343434');
 
 -- --------------------------------------------------------
 
@@ -126,7 +125,7 @@ CREATE TABLE `tb_tagihan` (
   `tgl_bayar` date NOT NULL,
   `status_bayar` int(11) NOT NULL,
   `no_invoice` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -142,14 +141,15 @@ CREATE TABLE `tb_user` (
   `level` varchar(30) NOT NULL,
   `foto` varchar(255) NOT NULL,
   `id_pelanggan` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tb_user`
 --
 
 INSERT INTO `tb_user` (`id`, `username`, `nama_user`, `password`, `level`, `foto`, `id_pelanggan`) VALUES
-(1, 'admin', 'Admin', 'admin', 'admin', 'avatar.png', 0);
+(1, 'admin', 'Admin', 'admin', 'admin', 'avatar.png', 0),
+(8, 'rossi', 'rosi', '12345', 'user', 'admin.png', 7);
 
 --
 -- Indexes for dumped tables
@@ -193,31 +193,31 @@ ALTER TABLE `tb_user`
 -- AUTO_INCREMENT for table `tb_kas`
 --
 ALTER TABLE `tb_kas`
-  MODIFY `id_kas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+  MODIFY `id_kas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 
 --
 -- AUTO_INCREMENT for table `tb_paket`
 --
 ALTER TABLE `tb_paket`
-  MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tb_pelanggan`
 --
 ALTER TABLE `tb_pelanggan`
-  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tb_tagihan`
 --
 ALTER TABLE `tb_tagihan`
-  MODIFY `id_tagihan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id_tagihan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
